c247afbcf2915b8fe3a59ef7ed88bd805787a4e2
NicolasMeseguer <nicolasmeseguer@icloud.com>
2022-06-05 16:51:29 +0200

    Header + Lógica del JS preparada
    Ahora se puede comenzar a maquetar las distintas secciones y los textos.

---
+++ assets/js/main.js
@@ -0,0 +1,118 @@
+// About Me content is the one by default shown
+$('#educationContent').hide();
+$('#publicationsContent').hide();
+$('#experienceContent').hide();
+$('#projectsContent').hide();
+
+$(document).ready(function(){
+
+	// Handle 'About Me' content
+	$('#aboutme').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#aboutmeContent');
+		}
+
+	});
+
+	// Handle 'Education' content
+	$('#education').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#educationContent');
+		}
+	});
+
+	// Handle 'Publications' content
+	$('#publications').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#publicationsContent');
+		}
+	});
+
+	// Handle 'Experience' content
+	$('#experience').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#experienceContent');
+		}
+	});
+
+	// Handle 'Projects' content
+	$('#projects').click(function(e) {
+
+		// If the div has already the class active, no need to reload the divs...
+		if(!$(e.target).hasClass('active')) {
+			// Update navbar
+			clearActiveLinks();
+			activateLink(e);
+
+			// Hide other contents
+			clearActiveDivs();
+
+			// Show current content
+			activateDiv('#projectsContent');
+		}
+	});
+
+
+});
+
+function clearActiveLinks() {
+	$('#navbarList .nav-item .nav-link').each(function() {
+		$(this).removeClass('active');
+	});
+}
+
+function clearActiveDivs() {
+	$('.container .content .active').each(function() {
+		$(this).removeClass('active');
+		$(this).hide();
+	});
+}
+
+function activateLink(e) {
+	$(e.target).addClass('active');
+}
+
+function activateDiv(divId) {
+	$(divId).addClass('active');
+	$(divId).show();
+}
